5f01e9543f0be549dcd8be275bac2f6bb0169742
sigmaligmaboy <ezekieldunning08@Outlook.com>
2026-05-13 18:49:36 +1000

    Update Library.lua

--- Obsidian-9e42038be2cedddcfd0d764ac0ebf1bcb146b040/Library.lua
+++ Obsidian-9e42038be2cedddcfd0d764ac0ebf1bcb146b040/Library.lua
@@ -3252,12 +3252,12 @@ do
     end
 
     function Funcs:AddTooltip(Text, DisabledText)
-        local HoverInstance = self.Holder or self.Base or self.TextLabel
+        local HoverInstance = self.ButtonHolder or self.Holder or self.Base or self.TextLabel
         if HoverInstance then
             return Library:AddTooltip(Text, DisabledText, HoverInstance)
         end
     end
-
+    
     function AddDropdownTooltips(dropdown, tooltips)
         -- tooltips = { ["ValueName"] = "Tooltip text", ... }
         
